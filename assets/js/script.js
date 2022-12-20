1990e4e62b38d34872ec86e80ecc55bd05d76cd8
timothymichaelcook <tmc295@email.vccs.edu>
2022-12-20 17:31:14 -0500

    working on fetch response, set up openweather account and generated api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-api.openweathermap.org/data/2.5/forecast?q=london&appid=847ffadac8f9704104c4582fe8936ffd
+/* api.openweathermap.org/data/2.5/forecast?q=london&appid=847ffadac8f9704104c4582fe8936ffd */
 
 
 function getInfo() {
@@ -7,5 +7,11 @@ function getInfo() {
   cityName.innerHTML = "Results for: " + newName.value
 }
 
-fetch("api.openweathermap.org/data/2.5/forecast?q=london&appid=847ffadac8f9704104c4582fe8936ffd")
+fetch("api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=847ffadac8f9704104c4582fe8936ffd")
+  .then(response => response.json)
+  .then(data => {
+    for(i = 0; i < 5; i++) {
+      document.getElementById("day" + (i + 1) + "Min:").innerHTML ="Min:" + Number(data.list[i].main.temp_min - 288.53).toFixed(1)+"°"
+    }
+  })
 
