9d05db9fc9801d968120dd114128a83bf11b901d
timothymichaelcook <tmc295@email.vccs.edu>
2022-12-21 17:23:10 -0500

    finished comments in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,71 @@
 // DECLARING FUNCTION GETWEATHER
 function getWeather() {
-  // DECLARING VARIBALE, SETTING IT TO DOCUMENT OBJECT WITH ID CITYINPUT
-  var newName = document.getElementById("cityInput");
-  // DECLARING VARIBALE, SETTING IT TO DOCUMENT OBJECT WITH ID CITYNAME
-  var cityName = document.getElementById("cityName");
-  // ASSIGNING VARIBALE, SETTING IT TO DOCUMENT OBJECT WITH ID CITYNAME
-  cityName.innerHTML = "Search Results: " + newName.value;
+  // DECLARING VARIBALE CITYINPUT, SETTING IT TO DOCUMENT OBJECT WITH ID CITYINPUT
+  var cityInput = document.getElementById("cityInput");
+  // DECLARING VARIBALE CITYRESULT, SETTING IT TO DOCUMENT OBJECT WITH ID CITYRESULT
+  var cityResult = document.getElementById("cityResult");
+  // ASSIGNING VARIBALE CITYINPUT VALUE TO CITYRESULT INNERHTML
+  cityResult.innerHTML = "Search Results: " + cityInput.value;
 
-fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=32ba0bfed592484379e51106cef3f204')
+// FETCH CALL TO API WITH QUERY CITYINPUT VALUE
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+cityInput.value+'&appid=32ba0bfed592484379e51106cef3f204')
+//CONVERT API RESPONSE TO JSON FORMAT
 .then(response => response.json())
+//RETURNS DATA
 .then(data => {
 
-  //Getting the min and max values for each day
+  // FOR LOOP RUNS 5 TIMES
   for(i = 0; i<5; i++){
-      document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
-      //Number(1.3450001).toFixed(2); // 1.35
+      //GETS DATA FROM TEMP_MIN NODE, CONVERTS TO F AND CONCATS STRING WITH DEGREE SYMBOL AND MIN:, ASSIGNS TO VARIABLE DAY#MIN, SETS TO DOCUMENT OBJECT ID, LOOPS 5X
+      document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(1.8*(data.list[i].main.temp_min - 273)+32).toFixed(1)+ "°";
   }
-
+  // FOR LOOP RUNS 5 TIMES
   for(i = 0; i<5; i++){
-      document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
+      //GETS DATA FROM TEMP_MAX NODE, CONVERTS TO F AND CONCATS STRING WITH DEGREE SYMBOL AND MIN:, ASSIGNS TO VARIABLE DAY#MAX, SETS TO DOCUMENT OBJECT ID, LOOPS 5X
+      document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(1.8*(data.list[i].main.max - 273)+32).toFixed(2) + "°";
   }
   //------------------------------------------------------------
 
-  //Getting Weather Icons
+  // FOR LOOP RUNS 5 TIMES
    for(i = 0; i<5; i++){
-      document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
-      data.list[i].weather[0].icon
-      +".png";
+      //GETS DATA FROM ICON NODE, CONCATS URL AND .PNG TO DATA, ASSIGNS TO IMG#, SETS TO DOCUMENT OBJECT ID, LOOPS 5X
+      document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+ data.list[i].weather[0].icon +".png";
   }
-  //------------------------------------------------------------
-  console.log(data)
 
 
 })
-
+//ERROR CATCH IF API CALL DOESN'T WORK
 .catch(err => alert("Please Try Again!"))
 }
 
+
+// DECLARE FUNCTION DEFAULTSCREEN
 function DefaultScreen(){
-  document.getElementById("cityInput").defaultValue = "Miami";
+  // ASSIGNING MIAMI AS DEFAULT VALUE OF ELEMENT WITH ID CITYINPUT
+ document.getElementById("cityInput").defaultValue = "Miami";
+  // CALL GETWEATHER FUNCTION
   getWeather();
 }
 
 
-//Getting and displaying the text for the upcoming five days of the week
-var d = new Date();
+// GETTING AND UPDATE TEXT 
+var days = new Date();
 var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
 
-//Function to get the correct integer for the index of the days array
+// DECLARE FUNCTION CHECKDAY WITH PARAMENTER OF DAY
 function CheckDay(day){
-  if(day + d.getDay() > 6){
-      return day + d.getDay() - 7;
+  // CONDITIONAL 
+  if(day + DynamicsCompressorNode.getDay() > 6){
+    // IF DAY
+      return day + days.getDay() - 7;
   }
   else{
-      return day + d.getDay();
+      //
+      return day + days.getDay();
   }
 }
-
+  // FOR LOOP RUNS 5 TIMES
   for(i = 0; i<5; i++){
+    // WEEKDAY ARRAY ITEM IS ASSIGNED TO THE ELEMENT ID OF DAY APPENDED WTIH THE DAY #, PRODUCING AN ARRAY OF 5 DAYS IN ORDER
       document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
   }
