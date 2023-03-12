0afda96a27a04c4d0c329b57f51fee11a36a27c1
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-11 19:26:08 -0500

    syntax error fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,5 +13,24 @@ var cityName = '';
 
 
 
-
 // EVENT LISTENER SECTION
+// Event listener on search button, waiting for click event from user
+$('#searchBtn').on('click', function(event) {
+  // .preventDefault method prevents page from reloading
+  event.preventDefault();
+  //
+  cityName = $('#city-input').value();
+  // Call function searchFunction
+  searchFunction();
+  // Conditional statement checking if cityName variable is equal in data and type of user input
+  if (cityName === '') {  
+    return;
+  }
+
+  localStorage.setItem('lastcity', cityName);
+  // Console log cityName variable
+  console.log(cityName);
+  // Console log cities variable
+  console.log(cities);
+});
+
